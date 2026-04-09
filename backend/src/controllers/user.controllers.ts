447d3a5791dcfbe5273c7e888a7ea7ac37e76303
GuramiDavitadze <guramidavitadze2002@gmail.com>
2026-04-09 21:48:54 +0400

    User registration is done

--- backend/src/controllers/user.controllers.ts
+++ backend/src/controllers/user.controllers.ts
@@ -4,18 +4,19 @@ import { hashPassword } from "../utils/passwdHelper";
 import { generateToken } from "../utils/jwtHelper";
 const registerController = async (req: Request, res: Response) => {
   try {
-    const { fullname, email, password } = req.body;
+    const { fullname, email, password, image, role } = req.body;
     const hashedPassword = await hashPassword(password);
-    const data = { fullname, email, password: hashedPassword };
+
+    const data = { fullname, email, password: hashedPassword, image, role };
     const resp = await userRegisterService(data);
     const token = generateToken({ id: resp.id, role: resp.role });
     res
       .status(200)
       .json({ message: "User registered successfully", token: token });
-  } catch (error:any) {
+  } catch (error: any) {
     if (error.code === "P2002") {
-      return res.status(409).json({message:"Email already exist!"})
-    }    
+      return res.status(409).json({ message: "Email already exist!" });
+    }
     return res.status(500).json({ message: "Internal Server error" });
   }
 };
--- backend/src/services/user.services.ts
+++ backend/src/services/user.services.ts
@@ -1,8 +1,11 @@
 import prisma from "../config/prisma";
+import { Role } from "../../generated/prisma";
 type user = {
   fullname: string;
   password: string;
   email: string;
+  image?: string | undefined;
+  role: Role | undefined;
 };
 export const userRegisterService = async (data: user) => {
   return await prisma.user.create({ data });

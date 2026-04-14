b2e613aaa44401bf67621dad87130d9f7a542c73
GuramiDavitadze <guramidavitadze2002@gmail.com>
2026-04-14 23:11:24 +0400

    feat: create files of ruser

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import userRouter from "./auth.routes";
+import authRouter from "./auth.routes";
 import categoryRouter from "./categories.routes";
 import productRouter from "./products.routes";
+import userRouter from "./user.routes";
 const rootRouter = Router();
 
-rootRouter.use("/auth", userRouter);
+rootRouter.use("/auth", authRouter);
 rootRouter.use("/category", categoryRouter);
 rootRouter.use("/product", productRouter);
+rootRouter.use("/user", userRouter);
 export { rootRouter };

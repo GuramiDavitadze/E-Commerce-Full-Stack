fc18c6fe538453c7f74d8df8511eee689f13e8a1
GuramiDavitadze <guramidavitadze2002@gmail.com>
2026-04-16 22:15:48 +0400

    fix: fetch not found product during order

--- backend/src/controllers/orders.controllers.ts
+++ backend/src/controllers/orders.controllers.ts
@@ -8,8 +8,9 @@ const createOrderController = async (req: Request, res: Response) => {
 
     res.status(201).json({ message: "Order Created Successfully", data: resp });
   } catch (error: any) {
-    console.log(error);
-
+    if (error.code === "P2003") {
+      return res.status(404).json({ message: "Product Not Found" });
+    }
     return res.status(500).json({ message: "Internal Server Error" });
   }
 };

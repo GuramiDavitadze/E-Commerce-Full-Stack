332425535c2cb075d8f02176d7454cb9b760ef84
GuramiDavitadze <guramidavitadze2002@gmail.com>
2026-04-16 16:24:00 +0400

    feat: full setup of order middleware

--- backend/src/middlewares/index.ts
+++ backend/src/middlewares/index.ts
@@ -3,3 +3,4 @@ export * from "./global.middleware";
 export * from "./auth.middlewares";
 export * from "./products.middlewares";
 export * from "./user.middlewares";
+export * from "./order.middlewares";
--- backend/src/middlewares/order.middlewares.ts
+++ backend/src/middlewares/order.middlewares.ts
@@ -1,5 +1,26 @@
-import { Request, Response, NextFunction } from 'express'
+import { Request, Response, NextFunction } from "express";
 
-const checkOrderMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+const checkOrderMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  if (!req.body) {
+    return res
+      .status(400)
+      .json({ message: "You need to provide products you want to order" });
+  }
+
+  const { products } = req.body;
+  if (!products) {
+    return res
+      .status(400)
+      .json({ message: "You need to provide products you want to order" });
+  }
+  if (products?.length < 1) {
+    return res.status(400).json({ message: "Order data is empty" });
+  }
     
-}
+  next();
+};
+export { checkOrderMiddleware };

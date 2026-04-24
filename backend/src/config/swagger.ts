323c573e7cb0068a0295f8ebc10286f18a7d6637
GuramiDavitadze <guramidavitadze2002@gmail.com>
2026-04-24 19:27:44 +0400

    feat: setup swagger

---
+++ backend/src/config/swagger.ts
@@ -0,0 +1,30 @@
+import swaggerJSDoc from "swagger-jsdoc";
+
+const options: swaggerJSDoc.Options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "E-commerce API",
+      version: "1.0.0",
+      description: "E-Commerce REST API documentation",
+    },
+    servers: [
+      { user: "http://localhost:3008", desciption: "Development server" },
+      {
+        user: "https://e-commerce-full-stack-production-2d56.up.railway.app",
+        desciption: "Production server server",
+      },
+    ],
+    components: {
+      securitySchemas: {
+        cookieAuth: {
+          type: "apiKey",
+          in: "cookie",
+          name: "accessToken",
+        },
+      },
+    },
+  },
+  apis: ["../routes/*.ts"],
+};
+export const swaggerSpec = swaggerJSDoc(options);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,8 @@ import cookieParser from "cookie-parser";
 import { rootRouter } from "./routes";
 import { checkAPISecretKey, generalLimiter } from "./middlewares";
 import { Request, Response } from "express";
+import swaggerUi from "swagger-ui-express";
+import { swaggerSpec } from "./config/swagger";
 dotenv.config();
 const app = express();
 const PORT = Number(process.env.PORT) || 3008;
@@ -19,6 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(generalLimiter);
 app.use("/api", checkAPISecretKey, rootRouter);
+app.use("/rest-api/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use("/health", (req: Request, res: Response) => {
   res.status(200).json({ status: "ok" });
 });
